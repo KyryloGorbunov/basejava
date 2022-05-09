91867b76645af8c434d8d2084d54f8961f74c9fd
Kyrylo Gorbunov <kyryl1504@gmail.com>
2022-05-09 21:43:26 +0200

    Modified files ArrayStorage and Resume.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,72 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int size;
 
-    void clear() {
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
+        if (size < storage.length) {
+            if (size == 0) {
+                storage[size] = r;
+                size++;
+            } else {
+                for (int i = 0; i < size; i++) {
+                    boolean isEqual = r.uuid.equals(storage[i].uuid);
+                    if (!isEqual && i == size - 1) {
+                        storage[size] = r;
+                        size++;
+                    } else if (isEqual) {
+                        break;
+                    }
+                }
+            }
+        }
     }
 
-    Resume get(String uuid) {
-        return null;
+    public Resume get(String uuid) {
+        Resume r = null;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                r = storage[i];
+                break;
+            }
+        }
+        return r;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                index = i;
+                break;
+            }
+        }
+        if (index >= 0) {
+            for (int i = index; i < size - 1; i++) {
+                storage[i] = storage[i + 1];
+            }
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        Resume[] resumes = Arrays.copyOf(storage, size);
+        return resumes;
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return size;
     }
 }
