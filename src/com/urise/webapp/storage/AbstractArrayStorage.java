d1cae703b1aa46e8999d6894c2740dd5bf09579c
Kyrylo Gorbunov <kyryl1504@gmail.com>
2022-05-23 22:50:03 +0200

    Add AbstractArrayStorage, SortedArrayStorage, Storage. Modified files in package storage.

---
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,64 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10000;
+
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index < 0) {
+            System.out.println("ERROR: Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+            System.out.println("Resume: " + r + " successfully updated.");
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.print("ERROR: Resume " + uuid + " not exist. ");
+            return null;
+        } else {
+            return storage[index];
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("ERROR: Resume " + uuid + " not exist");
+        } else {
+            System.arraycopy(storage, index + 1, storage, index, size);
+            size--;
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    protected abstract int getIndex(String uuid);
+}
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,80 +2,30 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    protected static final int STORAGE_LIMIT = 10000;
-    private final Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size;
+public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
+    @Override
     public void save(Resume r) {
-        if (findIndex(r.getUuid()) >= 0) {
-            System.out.println("ERROR: Resume already exist");
-        } else if (size >= storage.length) {
-            System.out.println("ERROR: Storage is already full");
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("ERROR: Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("ERROR: Storage overflow");
         } else {
             storage[size] = r;
             size++;
         }
     }
 
-    public void update (Resume r) {
-        int index = findIndex(r.getUuid());
-        if (index >= 0) {
-            storage[index] = r;
-        } else {
-            System.out.println("ERROR: Resume not found");
-        }
-    }
-
-    public Resume get(String uuid) {
-        int index = findIndex(uuid);
-        if (index >= 0) {
-            return storage[index];
-        } else {
-            System.out.print("ERROR: Resume not found. ");
-            return null;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = findIndex(uuid);
-        if (index >= 0) {
-            System.arraycopy(storage, index + 1, storage, index, size);
-            size--;
-        } else {
-            System.out.println("ERROR: Resume not found");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    private int findIndex(String uuid) {
-        int index = -1;
+    @Override
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
-                index = i;
-                break;
+                return i;
             }
         }
-        return index;
+        return -1;
     }
 }
