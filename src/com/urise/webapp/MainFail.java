ce071bfcb771c7dbff7e8008eaa71d3ab763a4af
Kyrylo Gorbunov <kyryl1504@gmail.com>
2022-06-30 00:35:44 +0200

    Lesson9 HW draft.

--- src/com/urise/webapp/MainFail.java
+++ src/com/urise/webapp/MainFail.java
@@ -3,6 +3,11 @@ package com.urise.webapp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class MainFail {
 
@@ -50,5 +55,36 @@ public class MainFail {
 
         System.out.println();
         recursiveRound(dir);
+
+        System.out.println("------------------------------------------------------------------");
+
+        Path path = dir.toPath();
+        try {
+            Files.walkFileTree(path, new FileVisitor<>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    System.out.println(dir.getFileName());
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    System.out.println(" " + file.getFileName());
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.TERMINATE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
