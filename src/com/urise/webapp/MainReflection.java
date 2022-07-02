6d41c11c81be822886fa4d935ff767171529f420
Kyrylo Gorbunov <kyryl1504@gmail.com>
2022-07-02 10:31:30 +0200

    Lesson10 json

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -15,7 +15,6 @@ public class MainReflection {
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
         Method method = resumeClass.getMethod("toString");
         Object result = method.invoke(r);
         System.out.println(result);
