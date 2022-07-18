305dfe1869207498e47b5b9d79d20baf773f1d74
Kyrylo Gorbunov <kyryl1504@gmail.com>
2022-07-18 21:53:02 +0200

    Lesson11 MainConcurrency, LazySingleton, HW11 DeadLockDemo.

---
+++ src/com/urise/webapp/MainConcurrency.java
@@ -0,0 +1,122 @@
+package com.urise.webapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainConcurrency {
+    public static final int THREADS_NUMBER = 10000;
+    private int counter;
+    private static final Object LOCK = new Object();
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+                throw new IllegalStateException();
+            }
+        };
+        thread0.start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+
+            private void inc() {
+                synchronized (this) {
+//                    counter++;
+                }
+            }
+
+        }).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(mainConcurrency.counter);
+    }
+
+    private synchronized void inc() {
+//        synchronized (this) {
+//        synchronized (MainConcurrency.class) {
+        counter++;
+//                wait();
+//                readFile
+//                ...
+//        }
+    }
+}
+
+class DeadLockDemo {
+    public static final Object LOCK_1 = new Object();
+    public static final Object LOCK_2 = new Object();
+
+
+    public static void main(String[] args) {
+        DeadThreadOne threadOne = new DeadThreadOne();
+        DeadThreadTwo threadTwo = new DeadThreadTwo();
+
+        threadOne.start();
+        threadTwo.start();
+    }
+
+    private static class DeadThreadOne extends Thread {
+
+        public void run() {
+            synchronized (LOCK_1) {
+                System.out.println("DeadThreadOne is holding Lock 1...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("DeadThreadOne is waiting for Lock 2...");
+                synchronized (LOCK_2) {
+                    System.out.println("DeadThreadOne  is holding Lock 1 and Lock 2...");
+                }
+            }
+        }
+    }
+
+    private static class DeadThreadTwo extends Thread {
+
+        public void run() {
+            synchronized (LOCK_2) {
+                System.out.println("DeadThreadTwo is holding LOCK 2...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("DeadThreadTwo is waiting for Lock 1...");
+                synchronized (LOCK_1) {
+                    System.out.println("DeadThreadOne  is holding Lock 1 and Lock 2...");
+                }
+            }
+        }
+    }
+}
