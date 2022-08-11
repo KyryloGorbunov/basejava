038750caff4bd658aa83a099909e207edbaebbd2
Kyrylo Gorbunov <kyryl1504@gmail.com>
2022-08-11 23:50:55 +0200

    Lesson15 build project to web-application;

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,4 +1,4 @@
-create table resume (
+CREATE TABLE resume (
   uuid      CHAR(36) PRIMARY KEY NOT NULL,
   full_name TEXT                 NOT NULL
 );
@@ -9,5 +9,16 @@ CREATE TABLE contact (
   type        TEXT     NOT NULL,
   value       TEXT     NOT NULL
 );
+
 CREATE UNIQUE INDEX contact_uuid_type_index
   ON contact (resume_uuid, type);
+
+CREATE TABLE section (
+  id          SERIAL,
+  resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  value       TEXT     NOT NULL
+)
+
+CREATE UNIQUE INDEX  section_uuid_type_index
+    on section (resume_uuid, type);
--- src/com/urise/webapp/model/SectionType.java
+++ src/com/urise/webapp/model/SectionType.java
@@ -6,7 +6,7 @@ public enum SectionType {
     ACHIEVEMENT("Achievements"),
     QUALIFICATIONS("Qualification"),
     EXPERIENCE("Work experience"),
-    EDUCATION("Образование");
+    EDUCATION("Education");
 
     private final String title;
 
